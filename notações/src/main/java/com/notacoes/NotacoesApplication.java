1963ef13117b5bdc5a8a8a3ef745b1df65e5f0d6
raphex04 <132359802+raphex04@users.noreply.github.com>
2024-03-14 12:05:17 -0300

    Add files via upload

---
+++ notações/src/main/java/com/notacoes/NotacoesApplication.java
@@ -0,0 +1,13 @@
+package com.notacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotacoesApplication.class, args);
+	}
+
+}
---
+++ notações/src/test/java/com/notacoes/NotacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
